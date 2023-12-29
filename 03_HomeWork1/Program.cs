54fc3591ea01b1fe559dee62b3dd24d5d7b2a3c5
Egor Saratov <npp_board@mail.ru>
2023-12-29 23:38:39 +0300

    Решил домашнюю работу HomeWork

---
+++ 03_HomeWork1/Program.cs
@@ -0,0 +1,67 @@
+﻿// Внутри класса Answer напишите метод CompareNumbers, который на вход принимает два числа и выводит, какое число большее, а какое меньшее.
+/*
+ if(firstNumber > secondNumber)
+    {
+        Console.Write($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");
+    }
+    if(firstNumber < secondNumber)
+    {
+        Console.Write($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
+    }
+    if(firstNumber == secondNumber)
+    {
+        Console.Write($"Введенные числа равны `{firstNumber}`");
+    }
+*/
+
+// Внутри класса Answer напишите метод FindMax, который принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+ int max = 0;
+   if (a > b)
+    {
+        if (a > c)
+        {
+          max = a;
+        }
+         if (c > a)
+         {
+           max = c;
+         }
+     }
+     else
+     {
+         if (b > c)
+         {
+          max = b;
+         }
+         if (c > b)
+         {
+         max = c;
+       }
+    }
+    return max;
+*/
+
+// Внутри класса Answer напишите метод CheckIfEven, который на вход принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка).
+/*
+if (number%2 == 0)
+    {
+      Console.WriteLine($"Число `{number}` чётное");
+    }
+    else
+    {
+      Console.WriteLine($"Число `{number}` нечётное");
+    }
+*/
+
+// Внутри класса Answer напишите метод PrintEvenNumbers, которая на вход принимает число (number), а на выходе выводит все чётные числа от 1 до number (включительно), 
+// после каждого числа должен быть знак пробела.
+/*
+for (int i = 1; i <= number; i++)
+    {
+      if (i%2 == 0)
+      {
+        Console.Write($"{i}\t");
+      }
+    }
+*/
