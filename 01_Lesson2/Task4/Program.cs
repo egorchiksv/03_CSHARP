be68cbfdecd0517944e110030924957d916b3c36
Egor Saratov <npp_board@mail.ru>
2024-01-09 22:08:34 +0300

    add Lesson2:Task4

--- 01_Lesson2/Task4/Program.cs
+++ 01_Lesson2/Task4/Program.cs
@@ -11,3 +11,24 @@ while (i < n)
     i = i + 1;
 }
 Console.WriteLine(max);
+
+max = array[0];
+for (int j = 1; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+Console.WriteLine(max);
+
+max = array[0];
+
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
