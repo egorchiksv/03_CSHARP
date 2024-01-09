f8d199c4e9a6831abd0a0b7e954a975cc2f56169
Egor Saratov <npp_board@mail.ru>
2024-01-09 21:52:18 +0300

    add Lesson2:Task4

---
+++ 01_Lesson2/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 5;
+int[] array = { 2, 4, 7, 3, 9 };
+int i = 0;
+int max = array[0];
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
