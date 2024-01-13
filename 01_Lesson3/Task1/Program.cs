59e6a6ea119aae57f9d2586535b438113ed4f3d2
Egor Saratov <npp_board@mail.ru>
2024-01-13 21:20:01 +0300

    add Lesson3:Task1

---
+++ 01_Lesson3/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denominator = c + d;
+    double result = numenator / denominator;
+    return result;
+}
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
