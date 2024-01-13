11312dc1940a35f1e1146fa3d063219a88e2c1ee
Egor Saratov <npp_board@mail.ru>
2024-01-13 22:00:17 +0300

    add Lesson3:Task2

---
+++ 01_Lesson3/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выво на экран квардратов чисел от 1 до N
+
+void PrintSquares(int limit)
+{
+    int i = 1;
+    while(i <= limit)
+    {
+        Console.Write($"{i*i} ");
+        i++;
+    }
+}
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
